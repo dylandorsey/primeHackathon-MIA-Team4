1b4f08cff6dbb775a7fa1cfa7a14781286501e24
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-08-06 19:20:04 -0500

    cleanup of unnecessary code for base

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 
 const express = require('express');
-require('dotenv').config();
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -13,9 +12,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
-const acuityRouter = require('./routes/acuity.router');
 const databaseRouter = require('./routes/database.router');
-const googleRouter = require('./routes/google.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -30,9 +27,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/acuity', acuityRouter);
 app.use('/api/data', databaseRouter);
-app.use('/api/google', googleRouter);
 
 // Serve static files
 app.use(express.static('build'));
